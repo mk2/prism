7bd7fff10ab7f29292af442ba9acfed2fa25e9b4
Harukao <mk2@mk2.red>
2015-06-02 00:07:11 +0900

    [FIX] github oauth周り修正

--- github_handlers.go
+++ github_handlers.go
@@ -1,30 +1,63 @@
 package prism
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+)
 
 func GithubOAuthHandlers(res http.ResponseWriter, req *http.Request) {
 
-	path := NewPath(req.URL.Path)
+	tokens := strings.Split(req.URL.Path, "/")
+	action := tokens[2]
 
-	if !path.hasID() {
-		githubOAuthInfoHandler(res, req)
-		return
-	}
+	switch action {
 
-	switch path.ID {
-	case "redirect":
+	case "login":
+		githubLoginHandler(res, req)
+
+	case "callback":
+		githubCallbackHandler(res, req)
+
 	}
 
 	RespondErr(res, req, http.StatusBadRequest, "invalid request")
 }
 
-func githubOAuthInfoHandler(res http.ResponseWriter, req *http.Request) {
+func githubLoginHandler(res http.ResponseWriter, req *http.Request) {
 
-	clientID := GetVar(req, "GithubClientID").(string)
+	var clientID, status string = GetVar(req, "GithubClientID").(string), "dummy"
 
-	Respond(res, req, http.StatusOK, map[string]string{
-		"oauth_entry_url": "https://github.com/login/oauth/authorize",
-		"client_id":       clientID,
-		"state":           "dummy",
-	})
+	loginURL := fmt.Sprintf("http://github.com/login/oauth/authorize?client_id=%s&status=%s", clientID, status)
+
+	res.Header().Set("Location", loginURL)
+	res.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func githubCallbackHandler(res http.ResponseWriter, req *http.Request) {
+
+	var code,
+		status,
+		clientID,
+		clientSecret,
+		atURL string = req.Form.Get("code"), req.Form.Get("status"), GetVar(req, "GithubClientID").(string), GetVar(req, "GithubClientSecret").(string), "https://github.com/login/oauth/access_token"
+
+	dbg.Printf("Status: %s", status)
+
+	q := url.Values{}
+	q.Set("client_id", clientID)
+	q.Set("client_secret", clientSecret)
+	q.Set("code", code)
+
+	atres, _ := http.PostForm(atURL, q)
+
+	bytes, _ := ioutil.ReadAll(atres.Body)
+
+	authRes, _ := url.ParseQuery(string(bytes))
+
+	accessToken := authRes.Get("access_token")
+
+	dbg.Printf("AccessToken: %s", accessToken)
 }
